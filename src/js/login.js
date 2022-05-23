20037aa771cb6a9e26c1576343cdc06d2983b42f
Sunghan Kim <80890062+s-ggul@users.noreply.github.com>
2022-05-23 16:47:54 +0900

    Update login.js

--- src/js/login.js
+++ src/js/login.js
@@ -1,7 +1,7 @@
 export default function Login({$app, initialState,onClick})
 {
     this.state = initialState;
-    this.$target = document.createElement('a');
+    this.$target = document.createElement('div');
     this.$target.id = 'custom-login-btn';
     $app.appendChild(this.$target);
 
@@ -15,11 +15,11 @@ export default function Login({$app, initialState,onClick})
     this.render = () => {
         if(!this.state)
         {
-        this.$target.innerHTML = `<img
+        this.$target.innerHTML = `<a><img
                                 src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
                                 width="222"
                                 alt="카카오 로그인 버튼"
-                                />`
+                                /></a>`
         }
         else{
             this.$target.style.display = 'none';
