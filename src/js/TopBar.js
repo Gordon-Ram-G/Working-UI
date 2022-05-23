277a3229782cd37fbd520d2184483ee539a373da
Sunghan Kim <80890062+s-ggul@users.noreply.github.com>
2022-05-23 16:19:15 +0900

    Update TopBar.js

--- src/js/TopBar.js
+++ src/js/TopBar.js
@@ -1,15 +1,15 @@
 export default function TopBar({$app, initialState})
 {
     this.state = initialState;
-    this.$target = document.createElement('nav');
-    this.$userTarget = document.createElement('div');
-    this.$userIngred = document.createElement('div');
-    this.$target.className = 'tops';
-    this.$userTarget.className = 'userTops';
-    this.$userIngred.className = 'userIngred';
+    this.$target = document.createElement('div');
+    //this.$userTarget = document.createElement('div');
+    //this.$userIngred = document.createElement('div');
+    this.$target.className = 'topbar';
+    //this.$userTarget.className = 'userTops';
+    //this.$userIngred.className = 'userIngred';
     $app.appendChild(this.$target);
-    $app.appendChild(this.$userTarget);
-    $app.appendChild(this.$userIngred);
+    //$app.appendChild(this.$userTarget);
+    //$app.appendChild(this.$userIngred);
 
     this.setState = (newState) => {
         this.state = newState;
@@ -22,53 +22,56 @@ export default function TopBar({$app, initialState})
             if(this.$target.innerHTML === '')
             {
                 this.$target.style.display = '';
-                this.$userTarget.style.display = '';
-                this.$userIngred.style.display = '';
-                this.$target.innerHTML = `<div id="burger_btn">
-                                        <img src='./src/assets/uil_align-justify.png' style="height:120px;width:120px;">
-                                    </div>
-                                    
-                                    <div id="logo">
-                                        <img src='./src/assets/squirrel.png' style="height:120px;width:120px;">
-                                        <div>Gordon-Ram-G</div>
-                                    </div>
+                //this.$userTarget.style.display = '';
+                //this.$userIngred.style.display = '';
+                this.$target.innerHTML = `
+                                    <div class = "top_top"> 
+                                      <div id="burger_btn">
+                                          <img src='./src/assets/uil_align-justify.png' style="height:120px;width:120px;">
+                                      </div>
 
-                                    <div id="search_btn">
-                                        <img src='./src/assets/uil_search.png' style="height:120px;width:120px;">
+                                      <div id="logo">
+                                          <img src='./src/assets/squirrel.png' style="height:120px;width:120px;">
+                                          <div>Gordon-Ram-G</div>
+                                      </div>
+
+                                      <div id="search_btn">
+                                          <img src='./src/assets/uil_search.png' style="height:120px;width:120px;">
+                                      </div>
                                     </div>
+                                    <div class="top_mid">
+                                      <div id="user_info">
+                                          <div id="user_id"><b>${localStorage.getItem('nickname')}</b>님</div>
+                                          <div id="comment">오늘은 어떤것을 먹어볼까요?</div>
+                                      </div>   
+                                      <div id="order_btn">
+                                          <button>
+                                              <img src='./src/assets/uil_shopping-cart-alt.png' style="height:120px;width:120px;">
+                                              <span>주문하기</span>
+                                          </button>
+                                      </div>
+                                    </div>
+                                    <div class="top_bottom">
+                                      <div id="Main_Ingred">
+                                          <div id="Ingred_Board">
+                                              <img src='./src/assets/uil_clipboard-notes.png' style="height:120px;width:120px;">
+                                          </div>
+                                          <div id="Ingred_Info">
+                                              ...재료들
+                                          </div>
+                                      </div>
+                                    </div>    
                                     `;
-            this.$userTarget.innerHTML = `<div id="user_info">
-                                            <div id="user_id"><b>${localStorage.getItem('nickname')}</b>님</div>
-                                            <div id="comment">오늘은 어떤것을 먹어볼까요?</div>
-                                        </div>   
-                                        <div id="order_btn">
-                                            <button>
-                                                <img src='./src/assets/uil_shopping-cart-alt.png' style="height:120px;width:120px;">
-                                                <span>주문하기</span>
-                                            </button>
-                                        </div>
-                                        `;
-
-            this.$userIngred.innerHTML = `   
-                                        <div id="Main_Ingred">
-                                            <div id="Ingred_Board">
-                                                <img src='./src/assets/uil_clipboard-notes.png' style="height:120px;width:120px;">
-                                            </div>
-                                            <div id="Ingred_Info">
-                                                ...재료들
-                                            </div>
-                                        </div>
-                                        `;
             }
             else{
                 this.$target.style.display = '';
-                this.$userTarget.style.display = '';
-                this.$userIngred.style.display = '';
+                //this.$userTarget.style.display = '';
+                //this.$userIngred.style.display = '';
             }
         }else{
             this.$target.style.display = 'none';
-            this.$userTarget.style.display = 'none';
-            this.$userIngred.style.display = 'none';
+            //this.$userTarget.style.display = 'none';
+            //this.$userIngred.style.display = 'none';
         }
     }
     
